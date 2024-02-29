cb2913853c7af2821838bcac92479107013e9b02
Keagen <keegenjt@gmail.com>
2024-02-29 10:35:45 -0500

    Added command line support for reading in files

--- blink.py
+++ blink.py
@@ -4,17 +4,14 @@ from editor import BlinkEditor
 import sys;
 
 if __name__ == "__main__":
-    file_path = None;
-    # TODO ignore other file paths for now
-    if len(sys.argv) > 1:
-        file_path = sys.argv[1];
-
     app = QApplication([])
-
     with open("./style.css", 'r') as file:
         app.setStyleSheet(file.read())
 
-    window = BlinkEditor(file_path)
+    window = BlinkEditor()
+    for file_path in sys.argv[1:]:
+        window.create_tab(file_path);
+
     window.show()
     app.exec_()
 
